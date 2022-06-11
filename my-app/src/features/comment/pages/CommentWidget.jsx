b3d21cc0b317fd91b3f1211766fb08920f10123a
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2022-06-11 09:48:09 +0530

    chore
    - setup tailwindcss

--- my-app/src/features/comment/pages/CommentWidget.jsx
+++ my-app/src/features/comment/pages/CommentWidget.jsx
@@ -10,7 +10,9 @@ const CommentWidget = () => {
         value={comment}
         onChange={(e) => setComment(e.target.value)}
       />
-      <button></button>
+      <button className="bg-emerald-500 hover:bg-emerald-400 text-white font-bold py-2 px-5 border-b-4 active:border-b-0 mb-5 border-emerald-700 hover:border-emerald-500 rounded uppercase mr-5">
+        post
+      </button>
     </div>
   );
 };
